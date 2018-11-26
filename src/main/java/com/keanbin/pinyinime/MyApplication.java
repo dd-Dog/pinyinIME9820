c5fd3ff8a652f446b68a7b90608d45026ffb6252
bianjb <bianjb@flyscale.cn>
2018-11-27 00:24:37 +0800

    添加MyApplication启动加载笔画输入法数据库

---
+++ src/main/java/com/keanbin/pinyinime/MyApplication.java
@@ -0,0 +1,20 @@
+package com.keanbin.pinyinime;
+
+import android.app.Application;
+import android.content.Intent;
+
+import com.keanbin.pinyinime.service.LoadService;
+
+/**
+ * Created by bian on 2018/11/27.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Intent loadIntent = new Intent(this, LoadService.class);
+        startService(loadIntent);
+    }
+}
